f2f6dfc6d4a06b134b22c0169eeaa055858df55e
Nazarov Dmitry <Difat1@inbox.ru>
2022-07-15 09:48:52 +0300

    work

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Wathes from "./components/Wathes";
+
+function App() {
+  return (
+    <div className="App">
+      <Wathes />
+    </div>
+  );
+}
+
+export default App;
